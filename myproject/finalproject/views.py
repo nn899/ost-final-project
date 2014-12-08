6a7c99bc79a0af6e567231c99cdcb06bea7c8756
Nikita <nn899@nyu.edu>
2014-12-08 00:26:18 -0500

    Sign out working fine for "edit question" as well

--- myproject/finalproject/views.py
+++ myproject/finalproject/views.py
@@ -118,3 +118,53 @@ def add_question_login_form(request):
 
     else:
         return render_to_response('finalproject/login_form.html', { 'context': context, 'questions': q, }, template.RequestContext(request))
+
+def edit_question_login_form(request, question_id=None):
+    q = models.Question.all().order('title')
+    current_time = datetime.datetime.now()
+    user = users.get_current_user()
+    login_url = users.create_login_url(request.path)
+    logout_url = users.create_logout_url(request.path)
+    context = {
+        'current_time': current_time,
+        'user': user,
+        'login_url': login_url,
+        'logout_url': logout_url,
+    }
+
+    if user:
+        if request.method == 'POST':
+            # The form was submitted.
+            if question_id:
+                # Fetch the existing Question and update it from the form.
+                question = models.Question.get_by_id(int(question_id))
+                form = QuestionForm(request.POST, instance=question)
+            else:
+                # Create a new Question based on the form.
+                form = QuestionForm(request.POST)
+
+            if form.is_valid():
+                question = form.save(commit=False)
+                question.put()
+                return HttpResponseRedirect('/questions/')
+            # else fall through to redisplay the form with error messages
+
+        else:
+            # The user wants to see the form.
+            if question_id:
+                # Show the form to edit an existing Question.
+                question = models.Question.get_by_id(int(question_id))
+                form = QuestionForm(instance=question)
+            else:
+                # Show the form to create a new Question.
+                form = QuestionForm()
+
+        return render_to_response('finalproject/question_form.html', {
+            'questions': q,
+            'question_id': question_id,
+            'form': form,
+            'context': context,
+        }, template.RequestContext(request))
+
+    else:
+        return render_to_response('finalproject/login_form.html', { 'context': context, 'questions': q, }, template.RequestContext(request))
