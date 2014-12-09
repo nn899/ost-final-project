9f19317f8f615723d19369634e965c924c7c7351
Nikita <nn899@nyu.edu>
2014-12-09 11:50:38 -0500

    Added the request handler and model for answers

--- myproject/finalproject/models.py
+++ myproject/finalproject/models.py
@@ -11,6 +11,20 @@ class Question(db.Model):
     short_question = db.StringProperty(multiline=True)
     question_text = db.TextProperty(required=True)
 
+class Answers(db.Model):
+    question = db.ReferenceProperty(Question, collection_name='answers')
+    created_by = db.UserProperty(auto_current_user_add=True)
+    date_created = db.DateTimeProperty(auto_now_add=True)
+    modified_by = db.UserProperty(auto_current_user=True)
+    #date_modified = db.DateTimeProperty(auto_now=True)
+    date_modified = db.DateTimeProperty()
+    answer_text = db.TextProperty(required=True)
+
+#class Question_votes(db.Model):
+#class Answer_votes(db.Model):
+#class Question_tags(db.Model):
+#class Question_followers(db.Model):
+
 class QuestionReview(db.Model):
     question = db.ReferenceProperty(Question, collection_name='reviews')
     review_author = db.UserProperty()
--- myproject/urls.py
+++ myproject/urls.py
@@ -19,4 +19,5 @@ urlpatterns = patterns('myproject',
     url(r'^questions/$', 'finalproject.views.home'),
     url(r'^questions/add_question$', 'finalproject.views.add_question_login_form'),
     url(r'^questions/edit_question/(\d*)$', 'finalproject.views.edit_question_login_form'),
+    url(r'^questions/answers/(\d*)$', 'finalproject.views.view_question'),
 )
