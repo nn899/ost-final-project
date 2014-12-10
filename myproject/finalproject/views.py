04f44716f3edc42e0997245a520b12bb4e6cedec
Nikita <nn899@nyu.edu>
2014-12-09 22:32:33 -0500

    Adding answers seems to be working fine now

--- myproject/finalproject/views.py
+++ myproject/finalproject/views.py
@@ -141,14 +141,8 @@ def edit_question_login_form(request, question_id=None):
 
 def view_question(request, question_id=None):
     q = models.Question
-#    question_id = db.Key(question_id)
-#    q.filter('__key__', question_id)
     q = q.get_by_id(int(question_id))
-#    a = models.Answers
-#    a.filter('question=', q)
-#   count = q.count()
     a = db.Query(models.Answers)
-#    a.question = q
     a.filter('question', q)
     count = a.count()
     current_time = datetime.datetime.now() + datetime.timedelta(hours=-5)
@@ -176,14 +170,8 @@ class AnswerForm(djangoforms.ModelForm):
 
 def add_answer_login_form(request, question_id=None):
     q = models.Question
-#    question_id = db.Key(question_id)
-#    q.filter('__key__', question_id)
     q = q.get_by_id(int(question_id))
-#    a = models.Answers
-#    a.filter('question=', q)
-#   count = q.count()
     a = db.Query(models.Answers)
-#    a.question = q
     a.filter('question', q)
     count = a.count()
     current_time = datetime.datetime.now() + datetime.timedelta(hours=-5)
@@ -228,9 +216,14 @@ def add_answer_login_form(request, question_id=None):
             'count': count,
         }, template.RequestContext(request))
 
-def edit_answer_login_form(request, question_id=None, answer_id=None):
-    q = models.Question.all().order('-date_modified')
-    count = q.count()
+def edit_answer_login_form(request, answer_id=None):
+    a = models.Answers
+    a = a.get_by_id(int(answer_id))
+    q = models.Question
+    q = q.get_by_id(int(a.question.key().id()))
+    all_a = db.Query(models.Answers)
+    all_a.filter('question', q)
+#    count = q.count()
     current_time = datetime.datetime.now() + datetime.timedelta(hours=-5)
     user = users.get_current_user()
     login_url = users.create_login_url(request.path)
@@ -244,39 +237,37 @@ def edit_answer_login_form(request, question_id=None, answer_id=None):
 
     if user:
         if request.method == 'POST':
-            if question_id:
+            if answer_id:
                 # Fetch the existing Question and update it from the form.
-                question = models.Question.get_by_id(int(question_id))
-                question.short_question = question.question_text[:500]
-                form = QuestionForm(request.POST, instance=question)
+                answer = models.Answers.get_by_id(int(answer_id))
+                form = AnswerForm(request.POST, instance=answer)
             else:
                 # Create a new Question based on the form.
-                form = QuestionForm(request.POST)
+                form = AnswerForm(request.POST)
 
             if form.is_valid():
-                question = form.save(commit=False)
-                question.short_question = question.question_text[:500]
-                #question.date_modified = question.date_modified + datetime.timedelta(hours=-5)
-                question.date_modified = current_time
-                question.put()
-                return HttpResponseRedirect('/questions')
+                answer = form.save(commit=False)
+                #question.short_question = question.question_text[:500]
+                answer.date_created = answer.date_created + datetime.timedelta(hours=-5)
+                answer.date_modified = current_time
+                answer.put()
+                return HttpResponseRedirect('/questions/%d' % answer.question.key().id())
             # else fall through to redisplay the form with error messages
 
         else:
             # The user wants to see the form.
-            if question_id:
+            if answer_id:
                 # Show the form to edit an existing Question.
-                question = models.Question.get_by_id(int(question_id))
-                question.short_question = question.question_text[:500]
-                form = QuestionForm(instance=question)
+                answer = models.Answers.get_by_id(int(answer_id))
+                form = AnswerForm(instance=answer)
             else:
                 # Show the form to create a new Question.
-                form = QuestionForm()
+                form = AnswerForm()
 
-        return render_to_response('finalproject/question_form.html', {
-            'questions': q,
-            'count': count,
-            'question_id': question_id,
+        return render_to_response('finalproject/answer_form.html', {
+            'answers': a,
+#            'count': count,
+            'answer_id': answer_id,
             'form': form,
             'context': context,
         }, template.RequestContext(request))
@@ -284,6 +275,7 @@ def edit_answer_login_form(request, question_id=None, answer_id=None):
     else:
         return render_to_response('finalproject/answer_login_form.html', {
             'context': context,
-            'questions': q,
-            'count': count,
+            'answers': a,
+            'all_answers': all_a,
+            'answer_id': answer_id,
         }, template.RequestContext(request))
