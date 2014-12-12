7cb59e01a3c4397e3a5add09b32ee564855ee199
Nikita <nn899@nyu.edu>
2014-12-12 16:57:54 -0500

    More testing for tags

--- myproject/finalproject/views.py
+++ myproject/finalproject/views.py
@@ -65,8 +65,8 @@ def add_question_login_form(request):
                 question.date_created = question.date_created + datetime.timedelta(hours=-5)
                 question.date_modified = current_time
                 if question_tmp.question_tag:
-                    #question.question_tags = [" "]
-                    question.question_tags = [str(question_tmp.question_tag)]
+                    question.question_tag = question_tmp.question_tag
+                    question.question_tags = question.question_tags.append(str(question_tmp.question_tag))
                 question.put()
                 return HttpResponseRedirect('/questions')
 
